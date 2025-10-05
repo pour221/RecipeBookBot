22c3461ff32d332a5826817d792947b221589d8c
Alexander <@>
2025-10-05 15:38:14 +0400

    configs structure

---
+++ data_sample/configs.py
@@ -0,0 +1,18 @@
+TOKEN = ''
+DB_PATH = ''
+FEEDBACK_CHAT_ID = int
+
+IMG_PATH = ''
+
+pics = {'main_menu': f'{IMG_PATH}/recipebook.png',
+        'empty_list': f'{IMG_PATH}/my_recipe_list.png',
+        'new_recipe' : f'{IMG_PATH}/new_recipe.png',
+        'my_recipe': f'{IMG_PATH}/my_recipe.png',
+        'adding_recipe': f'{IMG_PATH}/adding_new.png',
+        'edit': f'{IMG_PATH}/adding_new.png',
+        'recipe_list': f'{IMG_PATH}/my_recipe_list.png',
+        'create_collection': f'{IMG_PATH}/adding_new.png',
+        'collection_list': f'{IMG_PATH}/my_recipe_list.png',
+        'success':f'{IMG_PATH}/adding_new.png',
+        'unsuccess': f'{IMG_PATH}/adding_new.png',
+        'delete': f'{IMG_PATH}/adding_new.png'}
